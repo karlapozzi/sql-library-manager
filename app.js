d8fbab9752623ad6bf548b082a9374e2b88baf81
karla <karla@getharvest.com>
2021-09-24 16:44:33 -0700

    adjust error handling to match project instructions

--- app.js
+++ app.js
@@ -31,22 +31,23 @@ app.use(function(req, res, next) {
   const err = new Error();
   err.status = 404;
   err.message = "Sorry! We couldn't find the page you were looking for.";
-  next(err);
+  if (err.status === 404) {
+    res.render('page-not-found', {error: err});
+  } else {
+    next(err);
+  }
 });
 
 //global error handler
 app.use(function(err, req, res, next) {
-  res.locals.error = err;
-  res.locals.message = err.message;
-  res.status(err.status || 500);
-
-  //If the error is a 404, show that page, otherwise show the global error template
-  if (err.status === 404) {
-    res.render('page-not-found');
-  } else {
-    err.message = "Sorry! There was an unexpected error on the server.",
-    res.render('error');
+  if(!err.status) {
+    err.status = 500;
   }
+  if(err.message === 'error is not defined' || !err.message) {
+    err.message = "Sorry! There was an unexpected error on the server.";
+  }
+  console.error(`${err.status}: ${err.message}`);
+  res.render('error', {error: err});
 });
 
 module.exports = app;

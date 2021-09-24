07ae503412ed57a7cc21bd99d1ece0013aa276bf
karla <karla@getharvest.com>
2021-09-24 15:48:15 -0700

    Add error handling

--- app.js
+++ app.js
@@ -15,31 +15,38 @@ sequelize.sync();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static('public'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+//404 error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  const err = new Error();
+  err.status = 404;
+  err.message = "Sorry! We couldn't find the page you were looking for.";
+  next(err);
 });
 
-// error handler
+//global error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  res.locals.error = err;
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+
+  //If the error is a 404, show that page, otherwise show the global error template
+  if (err.status === 404) {
+    res.render('page-not-found');
+  } else {
+    err.message = "Sorry! There was an unexpected error on the server.",
+    res.render('error');
+  }
 });
 
 module.exports = app;

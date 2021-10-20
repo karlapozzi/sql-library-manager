0858a79dc7675e26e1fb23faf2939dde46bedf58
karla <karla@getharvest.com>
2021-10-20 14:53:02 -0700

    update error copy

--- models/book.js
+++ models/book.js
@@ -9,10 +9,10 @@ module.exports = (sequelize) => {
       allowNull: false,
       validate: {
         notNull: {
-          msg: 'Please provide a value for "title"'
+          msg: 'Title is required'
         },
         notEmpty: {
-          msg: 'Please provide a value for "title"'
+          msg: 'Title is required'
         }
       }
     },
@@ -21,10 +21,10 @@ module.exports = (sequelize) => {
       allowNull: false,
       validate: {
         notNull: {
-          msg: 'Please provide a value for "author"'
+          msg: 'Author is required'
         },
         notEmpty: {
-          msg: 'Please provide a value for "author"'
+          msg: 'Author is required'
         }
       }
     },
